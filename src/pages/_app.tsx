2f2ded55a6ff47d0f3f690cea23f1aa6c1902bab
rvdm2101 <rvdmolen2101@gmail.com>
2023-09-11 18:38:29 +0200

    Added sass and using the pages nextjs structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import Layout from '../components/Layout';
+import './globals.scss';
+import type { AppProps } from 'next/app';
+
+export default function App({
+  Component,
+  pageProps
+}: AppProps) {
+  return (
+      <Layout>
+    <Component {...pageProps} />
+      </Layout>
+  )
+}
